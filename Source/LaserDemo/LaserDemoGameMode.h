bfbcd6fec9255400c40daafaabfa8de79a0366c2
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-03-05 15:29:13 +0200

    initial commit

---
+++ Source/LaserDemo/LaserDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LaserDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALaserDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALaserDemoGameMode();
+};
+
+
+
